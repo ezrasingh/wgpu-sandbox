427cbebd3a3dd01a7ff9f94be01c9f9cecb740cc
ezrasingh <singhezra@gmail.com>
2024-08-10 06:28:45 +0000

    Deploying to gh-pages from @ ezrasingh/wgpu-sandbox@e9357ab9c1dd30a923fee3bebf40706ca93cf3cf 🚀

--- src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub mod app;
-pub mod examples;
-pub mod gpu;
--- src/main.rs
+++
@@ -1,7 +0,0 @@
-use leptos::*;
-use leptos_example::app::App;
-
-fn main() {
-    console_error_panic_hook::set_once();
-    mount_to_body(App)
-}
